7621c49bebfa0cf883a9563e690dec25779724bd
Nithin Reddy <nithinreddy34261@gmail.com>
2026-07-23 12:06:48 +0530

    fix cors

--- src/main/java/com/taxin60sec/backend/BackendApplication.java
+++ src/main/java/com/taxin60sec/backend/BackendApplication.java
@@ -3,6 +3,8 @@ package com.taxin60sec.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+
+@SpringBootApplication
 @ConfigurationPropertiesScan
 public class BackendApplication {
 
